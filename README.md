c599387634a976718896e82e9d5a09da89f6153c
unknown <a.colapietro2@campus.uniurb.it>
2018-05-16 10:11:10 +0200

    commit iniziale

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# progetto-pdt
+# Hotspots USA Gratuiti #
+[![Build Status](https://travis-ci.org/acolapietro2/progetto-pdt.svg?branch=master)](https://travis-ci.org/acolapietro2/progetto-pdt)
+
+## Progetto Piattaforme Digitali per la Gestione del Territorio ##
+
+### Appello: ###
+* Primo appello sessione estiva 2017/2018
+
+### Alunno: ###
+* [Antonio Colapietro](https://github.com/acolapietro2)
+
+-----------------------------------------------------
+
+## Descrizione ##
+
+Il progetto _Hotspots USA Gratuiti_ si pone come obbiettivi primari:
+* Ricerca degli hotspot gratuiti nelle città americane
+
+-----------------------------------------------------
+
+## Relazione ##
+
+Il progetto è composto da 2 obbiettivi principali:
+* Realizzazione di un API (GET) in NodeJS
+* Implementazione di un BotTelegram (Python)
---
+++ hotspots-api/server.js
@@ -0,0 +1,50 @@
+//Librerie
+const express = require('express');
+const request = require('request');
+const where = require("lodash.where");
+const app = express();
+
+// Link da dove vengono presi i dati
+const link_dati = "https://data.cityofnewyork.us/api/geospatial/a9we-mtpn?method=export&format=GeoJSON";
+
+// I dati saranno contenuti qua
+var data_store;
+
+// Descrizione API
+app.get('/', function (req, res) {
+  var dsc = '';
+  dsc = "Utilizzo:<br>";
+  dsc += "* /hotspots => Ritorna tutti gli hotspot<br>";
+  dsc += "* /hotspots/:city='nomedellacittà' => Ritorna solo gli hotspot nella città specificata<br>";
+  res.send(dsc);
+  // stato funzionamento server
+  res.status(200);
+});
+
+// Richiesta base
+app.get('/hotspots', function (req, res) {
+  res.send(data_store);
+  res.status(200);
+});
+
+// Richiesta con filtro
+app.get('/hotspots/:city', function (req, res) {
+  var filtered = data_store;
+  // Filtro i dati
+  filtered['features'] = where(data_store['features'], {
+    "properties": {
+      "city": req.params.city.replace(':city=', '')
+    }
+  });
+
+  res.send(filtered);
+  res.status(200);
+});
+
+const server = app.listen(process.env.PORT || 8000, function () {
+
+  // Carico i dati esterni
+  request(link_dati, function (error, response, body) {
+    data_store = JSON.parse(body);
+  });
+});
