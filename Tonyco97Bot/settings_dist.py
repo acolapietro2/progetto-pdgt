db024f90e4f3bba972f34e8f8e97da237a51afb5
unknown <a.colapietro2@campus.uniurb.it>
2018-05-17 11:15:44 +0200

    aggiornato il file per heroku e creato il bot di telegram

---
+++ Tonyco97Bot/settings_dist.py
@@ -0,0 +1,7 @@
+# Your Bot token (get it from BotFather)
+token = 'TOKEN'
+
+# Message send when user 'start' the bot
+start_msg = "Benvenuto su @Tonyco97Bot\n"\
+            "l'unico bot che ti indica i wifi liberi presenti nelle aree americane\n"\
+			"Buona permanenza da noi :) "
